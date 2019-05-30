f61b6d325ebd6bfa8b9f5d2d36f57aff7fca0816
unknown <@DESKTOP-J47RKBK.yyychis.com>
2019-05-30 22:35:43 +0800

    1

--- stockweb/timelyMarket/tests.py
+++
@@ -1,8 +0,0 @@
-import tushare as ts
-
-
-ts.set_token('38bb3cd1b6af2d75a7d7e506db8fd60354168642b400fa2104af81c5')
-pro = ts.pro_api()
-# df = ts.get_realtime_quotes('000581')
-df = pro.pro_bar(ts_code='000001.SZ', adj='qfq',freq='1min', start_date='20180101', end_date='20181011')
-print(df)
