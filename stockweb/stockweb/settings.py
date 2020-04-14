7abc64367020fbba225f841473d7cb7531099f8b
unknown <@DESKTOP-J47RKBK.yyychis.com>
2020-04-14 23:07:09 +0800

    1

--- stockweb/stockweb/settings.py
+++ stockweb/stockweb/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '^@8dh69#u#%&7jkj1k&^ze3)m7&o%620iz9pq=7-!!hiozgttl'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
